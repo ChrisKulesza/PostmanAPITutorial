a75d3ca333e1c7b1ea9814f39ecc86bbd7051372
Christian Kulesza <christian.kulesza@waits-gmbh.de>
2021-12-16 13:47:03 +0100

    miscellaneous

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PostmanAPI.Models;
+using PostmanAPI.Repositories;
+
+namespace PostmanAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly IPersonRepository _repository;
+
+        public PersonController(IPersonRepository repository)
+        {
+            _repository = repository;
+        }
+
+        #region Action methods
+        [HttpGet]
+        public async Task<IEnumerable<Person>> GetPersonAsync()
+        {
+            return await _repository.GetAllPersonAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Person>> GetPersonAsync(Guid id)
+        {
+            return await _repository.GetPersonByIdAsync(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Person>> PostPersonAsync([FromBody]Person person)
+        {
+            var newPerson = await _repository.CreatePersonAsync(person);
+            return CreatedAtAction(nameof(GetPersonAsync), new { id = newPerson.Id }, newPerson);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutPerson(Guid id, [FromBody] Person person)
+        {
+            if (id != person.Id)
+            {
+                return BadRequest();
+            }
+
+            await _repository.UpdatePersonAsync(person);
+
+            return NoContent();
+        }
+
+        #endregion
+    }
+}
